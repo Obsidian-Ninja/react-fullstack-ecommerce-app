464bdc7c3c69a15522fce9ff5e3f6f15a11f2d35
Obsidian_Ninja <gurvxnsh.gupta@gmail.com>
2026-07-14 00:09:58 +0530

    add best-seller products

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero.jsx'
 import LatestCollection from '../components/LatestCollection.jsx'
+import BestSeller from '../components/BestSeller.jsx'
 function Home() {
   return (
     <div>
       <Hero />
       < LatestCollection />
+      <BestSeller />
     </div>
   )
 }
