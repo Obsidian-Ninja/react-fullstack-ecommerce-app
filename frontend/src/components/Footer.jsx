30d0738ffa5cb88e6667437dcad899903aa3830d
Obsidian_Ninja <gurvxnsh.gupta@gmail.com>
2026-07-14 02:12:50 +0530

    adding the footer and policy component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { assets} from "../assets/assets"
 
 function Footer() {
   return (
     <div>
-      
+      <div className="flex flex-col sm:grid gap-14 my-10 mt-40 text-sm grid-cols-[3fr_1fr_1fr]">
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+                The biggest online retailer in the country. We hope you like our offerings. Thanks :)
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy poicy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+91 7087775674</li>
+                <li>contact@forever</li>
+            </ul>
+        </div>
+      </div>
+
+    <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2026@ forever.com - All Rights Reserved</p>
+    </div>
+
+
     </div>
   )
 }
