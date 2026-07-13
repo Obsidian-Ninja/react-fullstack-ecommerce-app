23d5c518b938344f3b09e7646083246f531ada16
Obsidian_Ninja <gurvxnsh.gupta@gmail.com>
2026-07-13 23:35:17 +0530

    Initial Commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../components/Hero.jsx'
+import LatestCollection from '../components/LatestCollection.jsx'
+function Home() {
+  return (
+    <div>
+      <Hero />
+      < LatestCollection />
+    </div>
+  )
+}
+
+export default Home
